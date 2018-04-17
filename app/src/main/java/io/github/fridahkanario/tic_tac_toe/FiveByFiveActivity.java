1be5d0f96cf743fd93a9fa868c27302c97a4664c
fridahkanario <kanariofridah11@gmail.com>
2018-04-17 22:12:54 +0300

    First Commit

---
+++ app/src/main/java/io/github/fridahkanario/tic_tac_toe/FiveByFiveActivity.java
@@ -0,0 +1,15 @@
+package io.github.fridahkanario.tic_tac_toe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.fridahkanario.tic_tac_toe.R;
+
+public class FiveByFiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_five_by_five);
+    }
+}
---
+++ app/src/main/java/io/github/fridahkanario/tic_tac_toe/MainActivity.java
@@ -0,0 +1,36 @@
+package io.github.fridahkanario.tic_tac_toe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void game3(View view) {
+        Intent intent = new Intent(this, ThreeByThreeActivity.class);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
+
+    public void game4(View view) {
+        Intent intent = new Intent(this, FourByFourActivity.class);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
+
+    public void game5(View view) {
+        Intent intent = new Intent(this, FiveByFiveActivity.class);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
+}
